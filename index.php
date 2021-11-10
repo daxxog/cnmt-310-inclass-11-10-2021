0de3e3c2b1328f5a6746245a08b20bb78a4a7c38
Volm, David <david@volminator.com>
2021-11-10 12:02:59 -0600

    CURLOPT_RETURNTRANSFER

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $url = "http://www.google.com";
 $ch = curl_init();
 curl_setopt($ch,
     CURLOPT_URL, $url);
-curl_exec($ch);
+curl_setopt($ch,
+    CURLOPT_RETURNTRANSFER, true);
+$return = curl_exec($ch);
+print $return;
 curl_close($ch);
 ?>
